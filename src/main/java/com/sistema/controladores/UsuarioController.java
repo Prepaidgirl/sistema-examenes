e305b280bfffb96463ec180f15302d369e8745fe
Prepaidgirl <andr.69cas@gmail.com>
2025-08-06 14:59:12 -0500

    hol

--- src/main/java/com/sistema/controladores/UsuarioController.java
+++ src/main/java/com/sistema/controladores/UsuarioController.java
@@ -3,14 +3,7 @@ package com.sistema.controladores;
 import java.util.HashSet;
 import java.util.Set;
 
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.sistema.Repositorio.RolRepository;
 import com.sistema.Servicios.UsuariosService;
@@ -23,13 +16,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/usuarios")
-
+@CrossOrigin(
+    origins = {"http://localhost:4200", "http://192.168.1.241:4200"},
+    allowCredentials = "true"
+)
 public class UsuarioController {
 
     private final UsuariosService usuariosService;
     private final RolRepository rolRepository;
 
-   
     public UsuarioController(UsuariosService usuariosService, RolRepository rolRepository) {
         this.usuariosService = usuariosService;
         this.rolRepository = rolRepository;
@@ -45,7 +40,6 @@ public class UsuarioController {
         usuario.setPerfil("default.png");
         Set<UsuarioRol> roles = new HashSet<>();
 
-        
         Rol rol = rolRepository.findById(2L).orElse(null);
         if (rol == null) {
             rol = new Rol();
@@ -57,7 +51,6 @@ public class UsuarioController {
         UsuarioRol usuarioRol = new UsuarioRol();
         usuarioRol.setUsuario(usuario);
         usuarioRol.setRol(rol);
-
         roles.add(usuarioRol);
 
         return usuariosService.guardarUsuario(usuario, roles);
